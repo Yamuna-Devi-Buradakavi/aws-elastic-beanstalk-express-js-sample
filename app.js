a7b23c7a10e892735accce73129eedac67b4cae3
Yamuna-Devi-Buradakavi <20A91A0512@aec.edu.in>
2023-03-17 06:12:45 +0530

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('First Mini!'));
+app.get('/', (req, res) => res.send('Piplining mini!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
